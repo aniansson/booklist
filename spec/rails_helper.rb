c3347cc5b4b0c20f001e86dab84802c7883b668c
aniansson <ebba@aniansson.se>
2017-10-28 11:24:54 +0200

    Clean up files that hound is compalaining about

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
 ActiveRecord::Migration.maintain_test_schema!
@@ -37,5 +37,4 @@ class Application < Rails::Application
     # Filter lines from Rails gems in backtraces.
     config.filter_rails_from_backtrace!
   end
-
 end
